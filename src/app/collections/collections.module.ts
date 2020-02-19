f81d06b4abe338079bf64e3ebe803cff033cbc90
Forstie <Ryan.Forstie1@T-Mobile.com>
2020-02-19 12:02:25 -0800

    Setup routing

--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -6,7 +6,6 @@ import { CollectionsHomeComponent } from "./collections-home/collections-home.co
 
 @NgModule({
   declarations: [CollectionsHomeComponent],
-  imports: [CommonModule, CollectionsRoutingModule],
-  exports: [CollectionsHomeComponent]
+  imports: [CommonModule, CollectionsRoutingModule]
 })
 export class CollectionsModule {}
