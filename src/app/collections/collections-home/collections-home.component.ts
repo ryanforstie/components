e48cec0fc6a5cca3bee10260138971369bf4d6e6
Forstie <Ryan.Forstie1@T-Mobile.com>
2020-02-21 13:01:02 -0800

    Add table component

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-collections-home',
-  templateUrl: './collections-home.component.html',
-  styleUrls: ['./collections-home.component.css']
+  selector: "app-collections-home",
+  templateUrl: "./collections-home.component.html",
+  styleUrls: ["./collections-home.component.css"]
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    { name: "James", age: 34, job: "Designer", employed: true },
+    { name: "Jill", age: 33, job: "Engineer", employed: true },
+    { name: "Ryan", age: 30, job: "Engineer", employed: false }
+  ];
+  headers = [
+    { key: "employed", label: "Employed" },
+    { key: "name", label: "Name" },
+    { key: "age", label: "Age" },
+    { key: "job", label: "Job" }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
