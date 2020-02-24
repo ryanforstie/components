0f941fe5ff59bea2be9a4a569baad021c58faf5e
Forstie <Ryan.Forstie1@T-Mobile.com>
2020-02-24 11:37:21 -0800

    Complete list component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ import { NotFoundComponent } from "./not-found/not-found.component";
 
 const routes: Routes = [
   {
+    path: "views",
+    loadChildren: () => import("./views/views.module").then(m => m.ViewsModule)
+  },
+  {
     path: "collections",
     loadChildren: () =>
       import("./collections/collections.module").then(m => m.CollectionsModule)
