b0f1978fcdfcae6fef966fa8ef88498f9241f08e
Forstie <Ryan.Forstie1@T-Mobile.com>
2020-02-26 14:17:04 -0800

    Complete accordion

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from "@angular/core";
 })
 export class ModsHomeComponent implements OnInit {
   modalOpen = false;
+  items = [
+    { title: "Why is the sky blue?", content: "Because" },
+    { title: "What does orange taste like?", content: "Orange" },
+    { title: "Why are you awesome?", content: "Because" }
+  ];
 
   constructor() {}
 
